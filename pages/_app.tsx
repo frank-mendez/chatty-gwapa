5483d2d8cdf2607bf15d9654893288485f35b8be
frankmendez <frankmendezresources@gmail.com>
2023-06-09 17:29:11 +0800

    Setup login and logout

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import type { AppProps } from 'next/app'
+import '../app/globals.css'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<UserProvider>
+			<Component {...pageProps} />
+		</UserProvider>
+	)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import Head from 'next/head'
+import Link from 'next/link'
 import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 const Home = () => {
-	return <div>Home</div>
+	const { user, error, isLoading } = useUser()
+
+	if (isLoading) return <div>Loading...</div>
+	if (error) return <div>{error.message}</div>
+	return (
+		<div>
+			<Head>
+				<title>Chatty Gwapa</title>
+			</Head>
+			<h1>Welcome</h1>
+			<div>
+				{user && (
+					<div>
+						{user.picture && user.name && <img src={user.picture} alt={user.name} />}
+						<h2>{user.name}</h2>
+						<p>{user.email}</p>
+						<Link href='/api/auth/logout'>Logout</Link>
+					</div>
+				)}
+				{!user && <Link href='/api/auth/login'>Login</Link>}
+			</div>
+		</div>
+	)
 }
 
 export default Home
