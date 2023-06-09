823177d1ba9aace98ac79a05aab43c8ee174cfc9
frankmendez <frankmendezresources@gmail.com>
2023-06-09 17:47:07 +0800

    Styled homepage with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,23 +9,26 @@ const Home = () => {
 	if (isLoading) return <div>Loading...</div>
 	if (error) return <div>{error.message}</div>
 	return (
-		<div>
+		<>
 			<Head>
-				<title>Chatty Gwapa</title>
+				<title>Chatty Gwapa - Login or Sign Up</title>
 			</Head>
-			<h1>Welcome</h1>
-			<div>
-				{user && (
-					<div>
-						{user.picture && user.name && <img src={user.picture} alt={user.name} />}
-						<h2>{user.name}</h2>
-						<p>{user.email}</p>
-						<Link href='/api/auth/logout'>Logout</Link>
-					</div>
-				)}
-				{!user && <Link href='/api/auth/login'>Login</Link>}
+			<div className='flex justify-center items-center min-h-screen w-full bg-gray-800 text-white text-center'>
+				<div>
+					{user && <Link href='/api/auth/logout'>Logout</Link>}
+					{!user && (
+						<>
+							<Link className='rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600' href='/api/auth/login'>
+								Log In
+							</Link>
+							<Link className='ml-2 rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600' href='/api/auth/login'>
+								Sign Up
+							</Link>
+						</>
+					)}
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
