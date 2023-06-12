941e8f80ec213da53435b9a446dea299ecb2b14e
frankmendez <frankmendezresources@gmail.com>
2023-06-12 23:06:04 +0800

    Finished implementing message component

--- components/Message/index.tsx
+++ components/Message/index.tsx
@@ -1,11 +1,21 @@
-import { MessageProps } from '@/pages/types'
+import { ChatRole, MessageProps } from '@/pages/types'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import Image from 'next/image'
 import React from 'react'
 
 const Message = (props: MessageProps) => {
 	const { role, content } = props
+	const { user } = useUser()
 	return (
-		<div className='grid grid-cols-[30px_1fr] gap-5 p-5'>
-			<div>avatar</div>
+		<div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === ChatRole.ASSISTANT ? 'bg-gray-600' : ''}`}>
+			<div>
+				{role === ChatRole.USER && user && (
+					<Image src={user.picture!} width={30} height={30} className='rounded-sm shadow-md shadow/black-50' alt='User avatar' />
+				)}
+				{role === ChatRole.ASSISTANT && (
+					<Image src={'/gwapa.png'} width={30} height={30} className='rounded-sm shadow-md shadow/black-50' alt='Gwapa avatar' />
+				)}
+			</div>
 			<div>{content}</div>
 		</div>
 	)
